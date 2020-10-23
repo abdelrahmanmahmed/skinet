9cfa6306e32d1f26c3c006690bca83c83ae30aaa
AbdelRahamn Ahmed <abdelrahman.m.ahmed@hotmail.com>
2020-10-23 10:37:06 +0800

    End of section 17

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './account/account.service';
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,29 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Skinet';
 
-  constructor(private basketService: BasketService) {}
+  constructor(private basketService: BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrntUser(token).subscribe(() => {
+      console.log('Loaded user');
+      // tslint:disable-next-line: no-shadowed-variable
+      }, error => {
+      console.log(error);
+      });
+  }
+
+  loadBasket(){
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
       this.basketService.getBasket(basketId).subscribe(() => {
         console.log('initialized basket');
+      // tslint:disable-next-line: no-shadowed-variable
       }, error => {
         console.log(error);
       });
